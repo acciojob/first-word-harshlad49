fa64238f496d641bc9e69781780ac4c46e360207
Harsh Lad <91070567+harshlad49@users.noreply.github.com>
2025-06-18 18:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading/trailing spaces
+  s = s.trim();
+  
+  // Find the index of the first space
+  const index = s.indexOf(' ');
+  
+  // If no space is found, return the entire string
+function firstWord(s) {
+  // Trim leading/trailing spaces
+  s = s.trim();
+  
+  // Find the index of the first space
+  const index = s.indexOf(' ');
+  
+  // If no space is found, return the entire string
+  if (index === -1) return s;
+
+  // Return substring up to the first space
+  return s.substring(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
